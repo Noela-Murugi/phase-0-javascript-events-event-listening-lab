f1b620dba03966ac14ff3b8785243a6766614650
Noela-Murugi <murugin254@gmail.com>
2022-08-21 11:33:28 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-function addingEventListener() {
+// function addingEventListener() {
+// }
+
+
+// document.querySelector('#input').addEventListener('click', function addingEventListener(){
+//     alert('I was clicked!');
+// });
+
+// function alertMe(){
+//     return alert('I was clicked!');
+// }
+
+function addingEventListener(){
+    document.querySelector('#input').addEventListener('click', addingEventListener);
+    return alert('I was clicked!');
 }
+// document.querySelector('#input').addEventListener('click', addingEventListener);
+
+
